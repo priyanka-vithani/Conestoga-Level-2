880b7501c600eb16b2adf8cb19099fcc1acde268
pyinka vithani <39754730+priyanka-vithani@users.noreply.github.com>
2024-03-06 15:57:14 -0500

    //-	Three (3) tests for a valid isosceles triangle

--- TriangleSolver/TriangleTestCases/UnitTest1.cs
+++ TriangleSolver/TriangleTestCases/UnitTest1.cs
@@ -20,4 +20,57 @@ public class Tests
         //Assert
         Assert.That(actual, Is.EqualTo(expected));
     }
+
+    //-	Three (3) tests for a valid isosceles triangle
+
+    [Test]
+    public void AnalyzeTriangle_In15and40and40_ReturnIsosceles()
+    {
+        //Arrange
+        int firstSide = 15;
+        int secondSide = 40;
+        int thirdSide = 40;
+
+        string expected = "An isosceles triangle is formed";
+
+        //Act
+        string actual = TriangleSolver.Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+        //Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void AnalyzeTriangle_In40and15and40_ReturnIsosceles()
+    {
+        //Arrange
+        int firstSide = 40;
+        int secondSide = 15;
+        int thirdSide = 40;
+
+        string expected = "An isosceles triangle is formed";
+
+        //Act
+        string actual = TriangleSolver.Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+        //Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void AnalyzeTriangle_In40and40and15_ReturnIsosceles()
+    {
+        //Arrange
+        int firstSide = 40;
+        int secondSide = 40;
+        int thirdSide = 15;
+
+        string expected = "An isosceles triangle is formed";
+
+        //Act
+        string actual = TriangleSolver.Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+        //Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
 }
