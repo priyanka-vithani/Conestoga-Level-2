1d10f0f9287b2e5c0075a9281a8d6b31d4fa65ec
pyinka vithani <39754730+priyanka-vithani@users.noreply.github.com>
2024-03-06 14:36:08 -0500

    initial commit

---
+++ TriangleSolver/TriangleSolver/Triangle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleSolver
+{
+    public static class Triangle
+    {
+        public static string AnalyzeTriangle(int firstSide, int secondSide, int thirdSide)
+        {
+            string output;
+            if ((firstSide == 0) || (secondSide == 0) || (thirdSide == 0))
+            {
+                output = "INVALID triangle - a zero was entered";
+                return output;
+            }
+
+                if (((firstSide + secondSide) > thirdSide) && ((firstSide + thirdSide) > secondSide) && ((secondSide + thirdSide) > firstSide))
+                {
+                    if ((firstSide == secondSide) && (firstSide == thirdSide) && (secondSide == thirdSide))
+                    {
+                        output = "A triangle is formed and it is an EQUILATERAL";
+                    }
+                    else if ((firstSide == secondSide) || (secondSide == thirdSide) || (thirdSide == firstSide))
+                    {
+                        output = "A triangle is formed and it is an ISOSCELES";
+
+                    }
+                    else
+                    {
+                        output = "A triangle is formed and it is a SCALENE";
+
+                    }
+
+
+                }
+                else
+                {
+                    output = "A triangle is not formed based on the input values";
+                }
+
+            
+            return output;
+        }
+    }
+}
